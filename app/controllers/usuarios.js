d30cef83a77a5d38db2175bd26da63b995392035
JJAV <info@impactotecnologico.net>
2018-10-16 11:06:05 +0200

    mejoras en retornos de error

--- app/controllers/usuarios.js
+++ app/controllers/usuarios.js
@@ -55,33 +55,45 @@ exports.insert = function(req, res) {
 
 	user.save(function(err, usuario) {
 		if(err) return res.status(500).send( err.message);
-    	res.status(200).jsonp(usuario);
+    	res.status(201).jsonp(usuario);
 	});
 };
 
 //PUT - Actualiza un registro de usuario
 exports.update = function(req, res) {
 	Usuario.findById(req.params.id, function(err, user) {
-		user.nombre   = req.body.nombre;
-		user.apellido    = req.body.apellido;
-		user.dni = req.body.dni;
-		user.email  = req.body.email;
-		user.password = req.body.password;
 
-		user.save(function(err) {
-			if(err) return res.status(500).send(err.message);
-      		res.status(200).jsonp(user);
-		});
+		if (user != null){
+			user.nombre   = req.body.nombre;
+			user.apellido    = req.body.apellido;
+			user.dni = req.body.dni;
+			user.email  = req.body.email;
+			user.password = req.body.password;
+
+			user.save(function(err) {
+				if(err) return res.status(500).send(err.message);
+				res.status(200).jsonp(user);
+			});
+		} else {
+			res.status(404).send({"error":"Usuario no encontrado"});
+		}
+
+		
 	});
 };
 
 //DELETE - Elimina un registro de usuario
 exports.delete = function(req, res) {
 	Usuario.findById(req.params.id, function(err, user) {
-		user.remove(function(err) {
-			if(err) return res.status(500).send(err.message);
-      		res.status(200).send();
-		})
+		if (user != null) {
+			user.remove(function(err) {
+				if(err) return res.status(500).send(err.message);
+				  res.status(204).send();
+			})
+		} else {
+			res.status(404).send({"error":"Usuario no encontrado"});
+		}
+		
 	});
 };
 
